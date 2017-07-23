0ade9e18bf33a66e5f73487547d11e6236351c00
Dylan Nichols <nichodo@g.toiohomai.ac.nz>
2017-07-24 11:17:25 +1200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace loops_pg02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
